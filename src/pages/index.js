b249a9f78d637fedca64fcf1c4f49e4932d49057
SassafrasAU <kalebndbowen@gmail.com>
2024-03-10 20:26:45 +1100

    Fix homepage -> onboarding issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/onboarding">
+            to="/docs/category/onboarding/">
             Documentation home
           </Link>
         </div>
